ff19147727a0e54206a33ed74b9b45e1ae445d17
Dejah Cooper <Dejah.C@icloud.com>
2024-11-20 15:47:44 -0500

    added PUT /invoices/id route

--- routes/invoices.js
+++ routes/invoices.js
@@ -37,7 +37,7 @@ router.post("/", async function(req, res, next) {
          if (!comp_code|| !amt) {
             return res.status(400).json({ error: "Missing required fields: comp_code, amt" });
         }
-        
+
         const results = await db.query(`INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING *`, [comp_code, amt]);
         
         return res.json({invoice : results.rows[0]})
@@ -47,11 +47,30 @@ router.post("/", async function(req, res, next) {
 })
 
 
-
-
-
 // **PUT /invoices/[id] :** Updates an invoice. If invoice cannot be found, returns a 404.
 // Needs to be passed in a JSON body of `{amt}` Returns: `{invoice: {id, comp_code, amt, paid, add_date, paid_date}}`
+router.put("/:id", async function(req, res, next) {
+    try {
+        const {amt} = req.body
+        // Ensure the required fields are provided
+         if (!amt) {
+            return res.status(400).json({ error: "Missing required field: amt" });
+        }
+
+        const results = await db.query(`UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *`, [amt, req.params.id])
+
+        // Handle case where the invoice is not found
+        if (results.rows.length === 0) {
+            return res.status(404).json({ error: "Company not found" });
+        }
+
+        return res.status(200).json({ company: results.rows[0] });        
+
+    } catch (e) {
+        next(e)
+    }
+})
+
 
 // **DELETE /invoices/[id] :** Deletes an invoice.If invoice cannot be found, returns a 404. Returns: `{status: "deleted"}` Also, one route from the previous part should be updated:
 

f88b32d1376b25d377981485fdf29c4c28050a06
Dejah Cooper <Dejah.C@icloud.com>
2024-11-20 20:23:35 -0500

    updated put route

--- routes/invoices.js
+++ routes/invoices.js
@@ -47,28 +47,48 @@ router.post("/", async function(req, res, next) {
 
 
 // Updates an invoice. If invoice cannot be found, returns a 404.
-router.put("/:id", async function(req, res, next) {
+// PUT /invoices/[id] : Updates an invoice. If invoice cannot be found, returns a 404.
+
+router.put("/:id", async function (req, res, next) {
     try {
-        const {amt} = req.body
-        // Ensure the required fields are provided
-         if (!amt) {
-            return res.status(400).json({ error: "Missing required field: amt" });
+        const { amt, paid } = req.body;
+        let paidDate = null; 
+
+        const currResult = await db.query(
+            `SELECT paid, paid_date
+             FROM invoices
+             WHERE id = $1`,
+            [req.params.id]
+        );
+
+        if (currResult.rows.length === 0) {
+            return res.status(404).json({ error: `Invoice ${req.params.id} not found` });
         }
 
-        const results = await db.query(`UPDATE invoices SET amt=$1 WHERE id=$2 RETURNING *`, [amt, req.params.id])
+        const currPaidDate = currResult.rows[0].paid_date;
 
-        // Handle case where the invoice is not found
-        if (results.rows.length === 0) {
-            return res.status(404).json({ error: "Company not found" });
+        if (!currPaidDate && paid) {
+            paidDate = new Date();
+        } else if (!paid) {
+            paidDate = null;
+        } else {
+            paidDate = currPaidDate;
         }
 
-        return res.status(200).json({ company: results.rows[0] });        
+        const result = await db.query(
+            `UPDATE invoices
+             SET amt = $1, paid = $2, paid_date = $3
+             WHERE id = $4
+             RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+            [amt, paid, paidDate, req.params.id]
+        );
+
+        return res.json({ invoice: result.rows[0] });
 
     } catch (e) {
-        next(e)
+        return next(e);
     }
-})
-
+});
 
 // Deletes an invoice. If invoice cannot be found, returns a 404. Returns: `{status: "deleted"}` 
 router.delete("/:id", async function(req, res, next) {
